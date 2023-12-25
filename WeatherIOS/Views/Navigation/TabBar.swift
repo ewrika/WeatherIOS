a1c0cc7299c5ef293ec3e605e000465281a2153a
ewrika <egorkaomsk_2003@mail.ru>
2023-12-25 16:47:39 +0600

    Дошел до таббара

---
+++ WeatherIOS/Views/Navigation/TabBar.swift
@@ -0,0 +1,18 @@
+//
+//  TabBar.swift
+//  WeatherIOS
+//
+//  Created by Георгий Борисов on 25.12.2023.
+//
+
+import SwiftUI
+
+struct TabBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TabBar()
+}
