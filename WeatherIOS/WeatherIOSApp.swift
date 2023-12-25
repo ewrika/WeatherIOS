23c5ebb1bc92eea61b52c16a84406a4d8f96c935
ewrika <egorkaomsk_2003@mail.ru>
2023-12-25 15:35:55 +0600

    Initial Commit

---
+++ WeatherIOS/WeatherIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherIOSApp.swift
+//  WeatherIOS
+//
+//  Created by Георгий Борисов on 25.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
